8263c8ab916546a395834dd848a4b91e34c02b9f
CaptainXD <CaptainXD@users.noreply.github.com>
2014-11-10 17:37:02 -0800

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
    $("#menuicon").click (function() {
       if (!open) {
          $("#menu").css("display", "block");
-         $("h1, #menu, #content").animate({left: "-=15%"}, 1000);
+         $("#bar, #menu, #content").animate({left: "-=15%"}, 1000);
          open = true;
       } else if (open) {
-         $("h1, #menu, #content").animate({left: "+=15%"}, 1000, function() {$("#menu").css("display", "none");});
+         $("#bar, #menu, #content").animate({left: "+=15%"}, 1000, function() {$("#menu").css("display", "none");});
          
          open = false;
       }
