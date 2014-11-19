f625c92773e4de2847cadf29ea80c028d607449f
CaptainXD <CaptainXD@users.noreply.github.com>
2014-11-18 18:44:05 -0800

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -5,10 +5,10 @@ $(document).ready(function() {
       if (!open) {
          $("html").css("overflow-x", "hidden");
          $("#menu").css("display", "block");
-         $("#screen").animate({left: "-=15%"}, 400);
+         $("#screen").animate({left: "-=200px"}, 400);
          open = true;
       } else if (open) {
-         $("#screen").animate({left: "+=15%"}, 400, function() {
+         $("#screen").animate({left: "+=200px"}, 400, function() {
             $("html").css("overflow-x", "hidden");
             $("#menu").css("display", "none");
          });
