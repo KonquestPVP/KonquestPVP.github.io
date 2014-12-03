136cdf39a9846bce60a08063edd5b440b808dc5b
CaptainXD <orcuttkids4him@gmail.com>
2014-12-02 21:07:55 -0800

    removed attempts and will leave alone for now

--- js/menu.js
+++ js/menu.js
@@ -16,12 +16,7 @@ $(document).ready(function() {
     $("#menuicon").on("mouseenter", hoverOn);
     $("#menuicon").on("mouseleave", hoverOff);
     $("#menuicon").on("touchmove", hoverOff);
-    $("#menuicon").on("click", function() {
-        if($("#menuicon").mouseleave()) {
-            hoverOff;
-        }
-    });
-    
+    $("#menuicon").on("click", hoverOff);
     button.click(function() {
         if (!open) {
             open = true;
