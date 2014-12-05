6a9eb4e75c163edb529820c5eef9e3b3eb0abf06
Kneelawk <jedidiah.pommert@gmail.com>
2014-12-04 19:45:03 -0800

    Possible menu borders and animation
    Please tell me what me what you guys think.

--- js/menu.js
+++ js/menu.js
@@ -1,40 +1,52 @@
 var open = false;
 
 function hoverOn() {
-    $("#menuicon").css("background", "#667");
+	$("#menuicon").css("background", "#667");
 }
 
 function hoverOff() {
-    $("#menuicon").css("background", "#333");
+	$("#menuicon").css("background", "#333");
 }
 
 function setMenuVisible(visible) {
-	if(visible) {
+	if (visible) {
 		open = true;
-		$("#overlay").fadeIn();
+		$("#overlay").fadeIn({
+			done : function(animation, jumpToEnd) {
+				$("#overlay ul").slideDown();
+			}
+		});
 	} else {
 		open = false;
-		$("#overlay").fadeOut();
+		$("#overlay ul").slideUp({
+			done : function(animation, jumpToEnd) {
+				$("#overlay").fadeOut();
+			}
+		});
 	}
 }
 
-$(document).ready(function() {
-    var menu = $("#menu");
-    var button = $("#menuicon");
-    
-    $("#menuicon").on("touchstart", hoverOn);
-    $("#menuicon").on("mouseenter", hoverOn);
-    $("#menuicon").on("mouseleave", hoverOff);
-    $("#menuicon").on("touchmove", hoverOff);
-    $("#menuicon").on("click", hoverOff);
-    
-    button.click(function() {
-        setMenuVisible(!open);
-    });
-    
-    $("#overlay").click(function(e) {
-        if (!menu.is(e.target) && menu.has(e.target).length === 0 && !button.is(e.target)) {
-            setMenuVisible(false);
-        }    
-    });
-});
+$(document).ready(
+		function() {
+			var menu = $("#menu");
+			var button = $("#menuicon");
+
+			$("#menuicon").on("touchstart", hoverOn);
+			$("#menuicon").on("mouseenter", hoverOn);
+			$("#menuicon").on("mouseleave", hoverOff);
+			$("#menuicon").on("touchmove", hoverOff);
+			$("#menuicon").on("click", hoverOff);
+
+			button.click(function() {
+				setMenuVisible(!open);
+			});
+
+			$("#overlay").click(
+					function(e) {
+						if (!menu.is(e.target)
+								&& menu.has(e.target).length === 0
+								&& !button.is(e.target)) {
+							setMenuVisible(false);
+						}
+					});
+		});
