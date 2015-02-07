0d58f85b3589c3a7948dbcc9889bc439811656c1
CaptainXD <orcuttkids4him@gmail.com>
2015-02-06 22:43:14 -0800

    updated content management system

---
+++ Konquest2/js/loader.js
@@ -0,0 +1,44 @@
+var files = [];
+var finished = true;
+var i = 0;
+
+function load() {
+    for(var j = 0; j < files.length; j++) {
+        jQuery.getJSON("json/" + files[j] + ".json", function(data) {
+            var info = [
+                "<h2>" + data.title + "</h2>",
+                "<p>" + data.date + "</p>",
+                "<p>" + data.author + "</p>",
+                "<p>" + data.content + "</p>"
+            ];
+            $("#content #info").append(info);
+        });
+    }
+}
+
+function go() {
+    $.get("json/" + i + ".json")
+    .done(function() {
+        finished = false;
+    })
+    .fail(function() {
+        finished = true;
+    })
+    .always(function() {
+        if(!finished) {
+            files.push(i);
+            i++;
+            go();
+        } else {
+            alert("The array stored the following values " + files);
+            alert("files.length contains " + files.length + " items");
+            load();
+        }
+    });
+
+}
+
+
+$(document).ready(function() {
+    go();
+});
