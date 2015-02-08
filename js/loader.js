87a7db49f060e6c7a7d04a1c4753199d84a2a285
Kneelawk <jedidiah.pommert@gmail.com>
2015-02-07 20:24:43 -0800

    Debugging

--- js/loader.js
+++ js/loader.js
@@ -1,19 +1,41 @@
 var files = [];
 var finished = true;
 var i = 0;
+var loadedPosts = [];
 
 function load() {
+    alert("loading json files");
+    jsonLoaded = 0;
     for(var j = 0; j < files.length; j++) {
         jQuery.getJSON("json/" + files[j] + ".json", function(data) {
-            var info = [
-                "<h2>" + data.title + "</h2>",
-                "<p>" + data.date + "</p>",
-                "<p>" + data.author + "</p>",
-                "<img src=\"img/" + data.author + ".png\" height=\"40\"  width=\"40\" />",
-                "<p>" + data.content + "</p>"
-            ];
-            $("#content #info").append(info);
+            alert("loading json: " + data);
+            loadedPosts.push(data);
+            if (loadedPosts.length == files.length) {
+                organizeAndPost();
+            }
         });
+    }
+}
+
+function organizeAndPost() {
+    alert("Done loading json");
+    loadedPosts.sort(function(a, b) {
+        var da = new Date(a.date);
+        var db = new Data(b.date);
+        if (da > db) return 1;
+        if (da < db) return -1;
+        else         return 0;
+    });
+    
+    for (var k = 0; k < loadedPosts.length; k++) {
+        var info = [
+            "<h2>" + data.title + "</h2>",
+            "<p>" + data.date + "</p>",
+            "<p>" + data.author + "</p>",
+            "<img src=\"img/" + data.author + ".png\" height=\"40\"  width=\"40\" />",
+            "<p>" + data.content + "</p>"
+        ];
+        $("#content #info").append(info);
     }
 }
 
